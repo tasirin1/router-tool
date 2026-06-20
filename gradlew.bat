359c2be9a58560d15fad21a7b622fb7e37a3fa6b
tasirin1 <tasirin1@users.noreply.github.com>
2026-06-20 15:22:30 +0700

    Fix build: auto-generate Gradle wrapper

---
+++ gradlew.bat
@@ -0,0 +1,13 @@
+@rem Gradle wrapper script
+@if "%DEBUG%" == "" @echo off
+@rem Set local scope
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+"%JAVA_HOME%/bin/java.exe" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
