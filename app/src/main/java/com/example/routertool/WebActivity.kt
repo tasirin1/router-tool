a0bd641c8a3f22c06304dcde0673f74f02f59857
tasirin <tasirin@users.noreply.github.com>
2026-06-21 05:28:16 +0700

    Fix missing View import in WebActivity

--- app/src/main/java/com/example/routertool/WebActivity.kt
+++ app/src/main/java/com/example/routertool/WebActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MotionEvent
+import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
 import android.webkit.WebView
